d6e2ae1c7f2ac30d87ae266a562e6011598ac7d2
Sneha L <Default user>
2017-12-13 11:53:05 -0500

    changed port listen in server

--- server.js
+++ server.js
@@ -71,4 +71,7 @@ con.connect(function(err) {
     });
 });
 
-app.listen(3000);
+//app.listen(3000);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
